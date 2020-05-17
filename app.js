6e9697a71ef7f5de3f0f9b3dfabc8c9a3946cdac
Andrew <addineen@gmail.com>
2020-05-17 14:03:43 -0700

    added views folder for MVC and barebones HTML for products and home tabs

--- app.js
+++ app.js
@@ -6,11 +6,16 @@ const bodyParser = require('body-parser');
 const app = express();
 
 const admin = require('./routes/admin');
-const shop = require('./routes/admin');
+const shop = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use(admin);
+app.use('/admin', admin);
 app.use(shop);
+
+app.use((req, res, next)=>
+{
+    res.status(404).send('<h1>Page not found</h1>');
+});
 //maintain event loop
 app.listen(3000);
