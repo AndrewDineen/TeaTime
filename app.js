0557d9523c5908aa34de59fe1d29fbce4fd0156c
Andrew <addineen@gmail.com>
2020-05-18 18:19:50 -0700

    added styling to the pages, server them statically with express

--- app.js
+++ app.js
@@ -2,20 +2,21 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const path = require('path');
 const app = express();
 
 const admin = require('./routes/admin');
 const shop = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
-
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', admin);
 app.use(shop);
 
 app.use((req, res, next)=>
-{
-    res.status(404).send('<h1>Page not found</h1>');
+{ 
+    res.status(404).sendFile(path.join(__dirname, 'views', 'page-not-found.html'));
 });
+
 //maintain event loop
 app.listen(3000);
