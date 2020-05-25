d0c5117f164027c30e8dc9fd1a7a9fa38e3dca91
Andrew <addineen@gmail.com>
2020-05-25 11:55:15 -0700

    Added some new tabs
    provided for the implementation of greater functionality such as adding products, managing cart items, etc.

--- controllers/products.js
+++ controllers/products.js
@@ -3,7 +3,7 @@ const Product = require('../models/product');
 exports.getAddProduct = (req, res, next) => 
 {
     //sends html to the page
-    res.render('add-product', {pageTitle: 'Add Product', path: '/admin/add-product'});
+    res.render('admin/add-product', {pageTitle: 'Add Product', path: '/admin/add-product'});
 };
 
 exports.postAddProduct = (req, res)=>
@@ -17,7 +17,7 @@ exports.postAddProduct = (req, res)=>
 exports.getProducts = (req, res, next) => 
 {
     Product.fetchAll(products => {
-        res.render('shop', {
+        res.render('shop/product-list', {
           prods: products,
           pageTitle: 'Shop',
           path: '/'
